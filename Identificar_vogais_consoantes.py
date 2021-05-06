fc5c7f5bb65d4730942f308f25bf771878c6f9d7
Arthur-84 <81167082+Arthur-84@users.noreply.github.com>
2021-05-06 16:57:36 -0300

    Create Identificar_vogais_consoantes.py
    Identificando vogais e consoantes por If, Elif e Else.

---
+++ Identificar_vogais_consoantes.py
@@ -0,0 +1,16 @@
+#Exercício 9
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+#lista = ["a","e","i","o","u"]
+ltr = str(input("Digite uma letra: "))
+if ltr == str("a"):
+  print(f"Vogal!")
+elif ltr == str("e"):
+  print(f"Vogal!")
+elif ltr == str("i"):
+  print(f"Vogal!")  
+elif ltr == str("o"):
+  print(f"Vogal!")
+elif ltr == str("u"):
+  print(f"Vogal!")  
+else:
+  print(f"Consoante!")
